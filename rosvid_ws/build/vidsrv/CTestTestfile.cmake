79d8366074ba3b2ad5a1f46db7dd84bb01bf2acf
litvintseva <litvintseva.ann@gmail.com>
2019-04-12 00:15:12 +0300

    launch for usb_cam_node

---
+++ rosvid_ws/build/vidsrv/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/n-31v/robotic-software/rosvid_ws/src/vidsrv
+# Build directory: /home/n-31v/robotic-software/rosvid_ws/build/vidsrv
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

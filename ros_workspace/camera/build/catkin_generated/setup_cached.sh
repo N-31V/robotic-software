d736f392c6b35ed1ac1518d9e84215d5d2e71660
Anna L <litvintseva.ann@gmail.com>
2019-04-13 14:27:00 +0300

    upd camera launch

---
+++ ros_workspace/camera/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/n-31v/robotic-software/ros_workspace/camera/build/devel:$CMAKE_PREFIX_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/n-31v/robotic-software/ros_workspace/camera/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/n-31v/robotic-software/ros_workspace/camera:/opt/ros/kinetic/share"
---
+++ ros_workspace/camera/build/gtest/CMakeFiles/gmock.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gmock.dir/usr/src/gtest/src/gtest-all.cc.o"
+  "CMakeFiles/gmock.dir/src/gmock-all.cc.o"
+  "/home/n-31v/robotic-software/ros_workspace/camera/lib/libgmock.pdb"
+  "/home/n-31v/robotic-software/ros_workspace/camera/lib/libgmock.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gmock.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

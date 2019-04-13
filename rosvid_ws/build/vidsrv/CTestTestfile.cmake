94a300f76253323880bd13123de0ec802b750623
Anna L <litvintseva.ann@gmail.com>
2019-04-13 18:14:55 +0300

    upd

--- rosvid_ws/build/vidsrv/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/n-31v/robotic-software/rosvid_ws/src/vidsrv
-# Build directory: /home/n-31v/robotic-software/rosvid_ws/build/vidsrv
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

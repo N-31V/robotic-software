5d065b130b6f110a8e90f83c1ac7bfb41eb9d458
Anna L <litvintseva.ann@gmail.com>
2019-04-23 21:00:54 +0300

    Добавлен код сервера на python и клиента на java для передачи jpg изображения

---
+++ Py/Image_server.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import socket
+
+sock = socket.socket()
+sock.bind(('localhost', 9090))
+go=True
+with open("Py/ggg.jpg", "wb+") as f:
+	while go:
+		sock.listen(1)
+		conn, addr = sock.accept()
+		print 'connected:', addr
+		while True:
+			data = conn.recv(1024)
+			if not data:
+				break
+		
+			if data == "q\n":
+				go = False
+				print (data)
+				conn.send(data.upper())
+			else:
+				f.write(data)
+				print('writting...') 
+		conn.close()
